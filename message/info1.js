b5175168475371bacf557a2be0a92b6669d9c798
takimtod <takimtod.com>
2021-12-11 12:15:28 +0800

    SelfBot3

---
+++ message/info1.js
@@ -0,0 +1,29 @@
+exports.bot = (gender,fake,nomerOwner)=> {
+return`
+*╭─▸* 『 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧𝗭  』
+
+🤖 Nama Bot: ${fake}
+👤 Owner : Takim tamvan
+☎️ Nomor Owner : ${nomerOwner}
+📍 Gender : ${gender}
+🌱 Umur : 17 Tahun
+🔥 Hoby : Recode SC
+
+
+*➻ JANGAN LUPA JOIN GRUP BOT*
+ ❍➤  *REVOLUTION*
+      • https://bit.ly/3IpHQlA
+ ❍➤  *EXTREAM*
+      • https://bit.ly/3GiD2wt
+
+*➻* SCRIPT BY*
+        Decode Denpa
+        X-Dev Team
+        Yogi PW
+        Hexagon
+        Dttaz
+
+*╰────────────*`
+}
+
+
---
+++ message/sewabot.js
@@ -0,0 +1,55 @@
+
+exports.dada = (prefix, settings, pushname, ucapanWaktu) => {
+return `${ucapanWaktu} kak ${pushname}
+Berikut adalah list harga untuk sewa botz
+
+*╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
+*┴*
+*│▸* 7 hari = 5k              
+*│▸* 30 hari = 20k      
+*│▸* Permanen = 30k                
+*┬*
+*╰────────────[*
+
+*╭─▸*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
+*┴*
+*│▸* Shopee : ${settings.Shopee}
+*│▸* Gopay : ${settings.Gopay}
+*│▸* Dana : ${settings.Dana}
+*│▸* Ovo : ${settings.Ovo}
+*┬*
+*╰────────────[*
+
+𝗡𝗢𝗧𝗘 : 
+
+Bot on 24 jam karena run Heroku
+tapi kadang Juga mati/slow respon
+karena Jaringan jemlek .
+
+Kalo mau sewa bisa chat
+owner Bot, khusus untuk free 
+sewa 1 hari bisa pake fitur
+${prefix}freeoneday <Link Gc>
+tanpa harus chat owner
+`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
